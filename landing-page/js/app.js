b77f5ac52f827838a30558ee075d7d1a349802e8
Cico <77722601+cico1111@users.noreply.github.com>
2022-08-06 10:23:01 +0900

    Update app.js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -27,18 +27,15 @@ const navigation = document.getElementById('navbar__list');
 // get all of the sections by classname
 const sections = document.querySelectorAll('section');
 // turn to arr  
-const sectionList=Array.from(sections)
+const sectionList=Array.from(sections);
 // get the active section
-let active_d = document.querySelector(".your-active-class")  
-
+let active_d = document.querySelector(".your-active-class");  
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
 
 /**
  * End Helper Functions
@@ -55,53 +52,88 @@ sections.forEach(section => {
 
 });
 
-
 // Add class 'active' to section when near top of viewport 
 
 //  is inviewport or not
-const isElementInViewport  = (el) =>{
+const isElementInViewport  = (element) =>{
 
-  const rect = el.getBoundingClientRect();
-  const top = rect.top 
-  const bottom = rect.bottom 
+  const rect = element.getBoundingClientRect();
+  const top = rect.top; 
+  const bottom = rect.bottom;
   //flag = 1:scroll down 
   //flag = -1:scroll up
-  let flag = 0 
+  let flag = 0 ;
   // if is inviewport ,return true  and flag
-  const isInView = top < 200  && bottom > el.offsetHeight/3
-  if(bottom<=el.offsetHeight/3){
-    flag = 1 
-  }
+  const isInView = top < 200  && bottom > element.offsetHeight/3;
+  if(bottom<=element.offsetHeight/3){
+    flag = 1 ;
+  };
   if(top > 200){
-    flag = -1 
-  }    
-  return {'inview':isInView, 'flag':flag} 
+    flag = -1 ;
+  };    
+  return {'inview':isInView, 'flag':flag} ;
+};
 
+// remove the active class
+const removeActive = (element) => {
+  element.classList.remove('your-active-class');
+  element.style.cssText = "background: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%);";
+};
+
+// adding the active class
+const addActive = (element) => {
+  element.classList.add('your-active-class');
+  element.style.cssText = "background-color: burlywood";
+  
+};
+
+//scroll listening function
+const scrollAction = () =>{
+  
+  // get the active element index in divlist
+  let index = sectionList.indexOf(active_d);
+  const InView = isElementInViewport(active_d);
+
+  //if is inviewport , add active class
+  if(InView.inview){  
+    addActive(active_d);
+
+  //if is not inviewport , remove active class
+  }else{  
+    removeActive(active_d) ; 
+
+    //check scroll up or down: (flag > 0)-> scroll down , (flag < 0)->scroll up 
+    if(InView.flag>0 && index < sectionList.length-1){
+      index=index+1; 
+    }; 
+    if(InView.flag < 0 && index >= 1){
+      index=index-1;
+    };
+    
+    active_d = sectionList[index];
+    addActive(active_d);
+    return;
+  };
+  // change the active section
+  active_d = sectionList[index];
 }
 
+const navbarAction = (event) =>{
+  //get the clicked navbar element
+  const activeNav = event.target；
+  //if the navbar has "link_click" (has been clicked) ,remove the class 
+  if(document.querySelector(".link_click")!= null){
+    document.querySelector(".link_click").classList.remove("link_click")；
+  }； 
+  // add the "link_click" class to clicked navbar  
+  activeNav.classList.add("link_click")；
+ 
+  //get the  clicked navbar's "href" value, "href" is the active section's id, use it to set the section to active
+  const section_id = activeNav.getAttribute("href");
+  addActive( document.querySelector(section_id))；
 
-window.addEventListener('scroll',(e) => {
-  // get the active element index in divlist
-  let index = sectionList.indexOf(active_d)
-  const InView = isElementInViewport(active_d)  
-  //if is inviewport , add active class
-  if(InView.inview){   
-    active_d.classList.add("your-active-class")  
-  //if is not inviewport , remove active class
-  }else{              
-    active_d.classList.remove("your-active-class")
-    
-  // flag > 0 , scroll down , (index+1) element is active
-  if(InView.flag>0 && index < sectionList.length-1) index=index+1 
-  // flag < 0 , scroll up , (index-1) element is active
-  if(InView.flag < 0 && index >= 1) index=index-1
-  
-  active_d = sectionList[index]
-  active_d.classList.add("your-active-class")
-  return
-  }
-  // change the active section
-  active_d = sectionList[index]
-
-
-})
+}
+// add listener on scroll
+window.addEventListener('scroll',scrollAction);
+//add listener on nav bar
+navigation.addEventListener('click', navbarAction);
